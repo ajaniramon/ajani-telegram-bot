022d8d0eccb59da3c9924a0c8fbf378ea7ead987
Ramón Martínez <rmartinezt@protonmail.com>
2021-03-12 23:04:43 +0100

    added ktor to avoid heroku killing my app

--- src/main/kotlin/es/heathcliff/ajanitestbot/Application.kt
+++ src/main/kotlin/es/heathcliff/ajanitestbot/Application.kt
@@ -2,8 +2,32 @@ package es.heathcliff.ajanitestbot
 
 import es.heathcliff.ajanitestbot.service.HttpService
 import es.heathcliff.ajanitestbot.service.TcpService
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 fun main() {
+        runApi()
+        runBot()
+}
+
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
+        }
+    }
+}
+
+fun runApi() {
+    embeddedServer(Netty, port = 80) {
+        module()
+    }.start(wait = false)
+}
+
+fun runBot() {
     val tcpService = TcpService()
     val httpService = HttpService()
 
